a1a1ddb70c5463623be9529624b48382d17d40e5
Jacques Guillory <jacquesguillory@Jacquess-MacBook-Pro.local>
2017-10-08 21:45:51 -0600

    initial commit

---
+++ BasicCard.js
@@ -0,0 +1,8 @@
+var inquirer = require("inquirer");
+var fs = require("fs");
+
+// constructor function for creating player objects
+function Basic(front, back) {
+    this.front = name;
+    this.back = back;
+}
